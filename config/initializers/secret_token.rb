ff25115de47ca6c7ba3cc9f2dcb605fb078e2c19
Ben Hughes <ben@railsgarden.com>
2011-02-18 19:15:42 -0800

    Initial Rails application with RSpec, Cucumber, Devise, HAML, SASS, and jQuery.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoworkingNomads::Application.config.secret_token = '2f6d60592390c22826e2e724f8fe31b715ee187e9b02dc6cb5ccefa91d7011b19ea10a2d51270fb763f7dcc08939c51c11a029004f76f2451ca8347953130583'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+CoworkingNomads::Application.routes.draw do
+  root :to => "home#index"
+end
