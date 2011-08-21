c6b1275f66befe8b352aafce3934d12e7b353b6f
Ben Hughes <ben@railsgarden.com>
2011-08-21 13:03:18 -0700

    Added landing page for production and access control for staging.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,24 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
   
+  if Rails.env.staging?
+    before_filter :authenticate_staging
+    
+    def authenticate_staging
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'oscar' && password == 'peterson'
+      end
+    end
+  end
+  
+  if Rails.env.production?
+    before_filter :force_landing_page
+    
+    def force_landing_page
+      unless request.path == "/"
+        redirect_to "/"
+        return false
+      end
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if Rails.env.production?
+      render :action => "landing", :layout => "landing"
+      return false
+    end
+    
     @cities = City.accessible_by(current_ability)
   end
 end
